e5ea386ff91126f3c39b9256d6d73dd0cbbeaae2
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2023-11-17 02:55:00 -0300

    fix: fazendo emulador funcionar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.matchmovie"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.matchmovie"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 

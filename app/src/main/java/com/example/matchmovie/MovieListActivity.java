ea82fdf43c18e3b62869c7bf3e873e04ed648089
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2023-11-17 20:05:20 -0300

    doc, feat, test: testando api

---
+++ app/src/main/java/com/example/matchmovie/MovieListActivity.java
@@ -0,0 +1,95 @@
+package com.example.matchmovie;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.matchmovie.models.MovieModel;
+import com.example.matchmovie.request.Servicey;
+import com.example.matchmovie.response.MovieSearchResponse;
+import com.example.matchmovie.utils.Credentials;
+import com.example.matchmovie.utils.MovieApi;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.matchmovie.databinding.ActivityMainBinding;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MovieListActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GetRetrofitResponse();
+            }
+        });
+    }
+
+    private void GetRetrofitResponse() {
+        MovieApi movieApi = Servicey.getMovieApi();
+        Call<MovieSearchResponse> responseCall = movieApi
+                .searchMovie(
+                        Credentials.API_KEY,
+                        "Oppenheimer",
+                        1
+                );
+        responseCall.enqueue(new Callback<MovieSearchResponse>() {
+            @Override
+            public void onResponse(Call<MovieSearchResponse> call, Response<MovieSearchResponse> response) {
+                if(response.code()==200){
+                    Log.v("tag", "the response" + response.body().toString());
+                    List<MovieModel> movies = new ArrayList<>(response.body().getMovies());
+                    for (MovieModel movie: movies){
+                        Log.v("tag", "A data de lançamento " + movie.getRelease_date());
+                    }
+                }
+                else{
+                    try{
+                        Log.v("tag", "Erro!" + response.errorBody().string());
+                    } catch (IOException e){
+                            e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MovieSearchResponse> call, Throwable t) {
+
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/matchmovie/utils/MovieApi.java
@@ -0,0 +1,16 @@
+package com.example.matchmovie.utils;
+
+import com.example.matchmovie.response.MovieSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MovieApi {
+    @GET
+    Call<MovieSearchResponse> searchMovie(
+            @Query("api_key") String key,
+            @Query("query") String query,
+            @Query("page") int page
+    );
+}

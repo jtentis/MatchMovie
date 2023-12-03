cab779aa483c2bbb6359353b8490335591369d08
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2023-12-02 23:55:28 -0300

    style: ajeitando nav bar

--- app/src/main/java/com/example/matchmovie/MovieListActivity.java
+++ app/src/main/java/com/example/matchmovie/MovieListActivity.java
@@ -38,7 +38,6 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.matchmovie.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationBarView;
 
 import java.io.IOException;
